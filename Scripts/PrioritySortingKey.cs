28e1fbdb96a1222cab91d4c1ef700074d8d48878
adamgryu <adamgryu@gmail.com>
2017-12-11 16:34:15 -0500

    added priority sorting key

---
+++ Scripts/PrioritySortingKey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// For use with SortedList.
+/// Sorts the items by priority, with using time as a tiebreaker.
+/// </summary>
+public struct PrioritySortingKey : IComparable {
+    private int priority;
+    private int time;
+
+    public PrioritySortingKey(int priority, int time) {
+        this.priority = priority;
+        this.time = time;
+    }
+
+    public int CompareTo(object obj) {
+        int compare = -1 * this.priority.CompareTo(((PrioritySortingKey)obj).priority);
+        if (compare == 0) {
+            return -1 * this.time.CompareTo(((PrioritySortingKey)obj).time);
+        }
+        return compare;
+    }
+
+    public override string ToString() {
+        return "(Priority: " + priority + ", Time: " + time + ")";
+    }
+}
