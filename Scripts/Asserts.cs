d50fee1dd80ce7b0239dbb7aba928852c92128be
adamgryu <adamgryu@gmail.com>
2018-08-26 09:33:44 -0400

    added asserts

--- Scripts/Asserts.cs
+++ Scripts/Asserts.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 public static class Asserts {
     public static void NotNull(object obj, string failureMessage = "") {
         if (obj == null) {
             throw new NullReferenceException("Not Null Assertion failed: " + failureMessage);
+        }
+    }
+
+    public static void Null(object obj, string failureMessage = "") {
+        if (obj != null) {
+            throw new Exception("Null Assertion failed: " + failureMessage);
         }
     }
 
@@ -16,9 +23,9 @@ public static class Asserts {
         }
     }
 
-    public static void Null(object obj, string failureMessage = "") {
-        if (obj != null) {
-            throw new Exception("Null Assertion failed: " + failureMessage);
+    public static void WeakAssertTrue(bool expression, string failureMessage) {
+        if (!expression) {
+            Debug.LogError("Weak Assertion failed: " + failureMessage);
         }
     }
 }
